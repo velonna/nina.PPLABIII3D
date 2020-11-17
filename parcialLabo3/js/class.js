3b0da880bf4bde0a2ddd1bc024804fb6759e2cb1
v nina <developers_v.nina@yahoo.com.ar>
2020-11-17 18:11:59 -0300

    primerparcial

--- parcialLabo3/js/class.js
+++ parcialLabo3/js/class.js
@@ -1,13 +1,16 @@
 
 
 export default class Anuncio {
-    constructor(id, titulo,transaccion,descripcion,precio) {
+    constructor(id, titulo,transaccion,descripcion,precio,puertas,kms,potencia) {
       this._id = id;
       this._titulo = titulo;
       this._transaccion = transaccion;
       this._descripcion = descripcion;
       this._precio = precio;
-      
+      this._puertas = puertas;
+      this._kms= kms;
+      this.potencia =potencia;
+           
     }
     agregar() {
         console.log(`hola ${this._id}`);
--- parcialLabo3/js/index.js
+++ parcialLabo3/js/index.js
@@ -2,43 +2,119 @@ import Anuncio,{a} from './class.js'
 import {autos} from './data.js'
 const spinner = document.getElementById("spinner");
 window.addEventListener("load",inicioParametros);
+    const frm = document.forms[0];
+
+    const txtTitulo = document.getElementById("txtTitulo");
+    const txtId = document.getElementById("idtxt");
+    const rdoVenta = document.getElementById("rdoV");
+    const rdoAlquiler = document.getElementById("rdoA");
+    const txtDesc = document.getElementById("txtDescripcion");
+    const txtPrecio = document.getElementById("txtPrecio");
+    const puertas = document.getElementById("txtPuerta");
+    const kms = document.getElementById("txtKm");
+    const potencia = document.getElementById("txtPotencia");
 
 function inicioParametros(){
-    
+  //  localStorage.clear();
     spinner.setAttribute("hidden", "");
     document.getElementById("btnGuadar").addEventListener("click",activarSpinner);
-   /* let ruedaSpinner =document.getElementById("spinner");
-    */
+    document.getElementById("btnCancelar").addEventListener("click",limpiarDatos);
+   
     console.log(a);    
 }
 function activarSpinner(){
  //  
     spinner.removeAttribute("hidden");
     window.setTimeout(function() {
-        activarTiempoDeEspera(1);
-      },1000);  
+        cargarDatos();
+      },900);  
 }
-function activarTiempoDeEspera(a){
-    
-    spinner.setAttribute("hidden", "");
-    const anuncio = new Anuncio("2","hola","transaccion","descr","23");
-    console.log(anuncio._id);
+
+
+
+function ingresar(e) {
+	
+    let tr = e.target.parentElement;
+    let nodos = tr.childNodes;
+    txtId.value = nodos[0].innerText;    
+	txtTitulo.value = nodos[1].innerText;
+    txtDesc.value = nodos[3].innerText;
+    txtPrecio.value = nodos[4].innerText;
+    kms.value = nodos[6].innerText;
+    potencia.value = nodos[7].innerText;
+    rdoVenta.value = true;
+    puertas.value = nodos[5].innerText;
+    document.getElementById("btnCancelar").hidden = false;
+    frm.addEventListener('submit', manejadorModificar);
+
+}
+
+function manejadorModificar(e) {
+    e.preventDefault();
+    let anuncio = cargarDatos(e.target, true);
+    modificarAnuncio(anuncio);
+}
+function limpiarDatos() {
+    txtTitulo.value = "";
+    txtId.value = "0";
+    rdoVenta.value = true;
+    rdoAlquiler.value = false;
+    txtDesc.value = "";
+    txtPrecio.value = "0";
+    puertas.value = "2";
+    kms.value="0";
+    potencia.value="0";
+}
+function cargarDatos(){
+    iniciarLocalStr("listarAutos");
+    const trans = rdoVenta.value == true?"Venta": "Alquiler";
+    const anuncio = new Anuncio("0",txtTitulo.value,trans,txtDesc.value,txtPrecio.value,puertas.value, kms.value,potencia.value);
+    console.log(anuncio);
     console.log(a);
+    spinner.setAttribute("hidden", "");
+    let lista = leerDatosDeLoco("listarAutos");
+    lista.push(anuncio);
+    borrarDatosDelLoco("listarAutos");
+    guardarDatosEnLoco("listarAutos", lista); 
+      
+}
+
+function guardarDatosEnLoco(nombre, array) {
+
+    localStorage.setItem(nombre, JSON.stringify(array));
 }
 
 
-console.log(autos);
+function iniciarLocalStr(unalist) {
+    let array = new Array();
+    if (localStorage.getItem(unalist) == null) {
+        guardarDatosEnLoco(unalist, array);
+    }
+}
+
 const btnTabla = document.getElementById('btnGuadar');
 btnTabla.addEventListener('click',function(){
     event.preventDefault();
     
     const divTabla = document.getElementById('divTabla');
- divTabla.appendChild(crearTabla(autos));
- 
+    Array.from(divTabla.childNodes).forEach( child => {divTabla.removeChild(child);});
+
+   // divTabla.appendChild(crearTabla(autos));
+    const datosAutos = leerDatosDeLoco("listarAutos");
+    divTabla.appendChild(crearTabla(datosAutos));
+
+    const eventosTds = document.getElementsByTagName("td");
+        for (var i = 0; i < eventosTds.length; i++) {
+            let td = eventosTds[i];
+            td.addEventListener('click', ingresar);
+        }
+
 
 });
+
 function crearTabla(lista){   
     const tabla = document.createElement('table');
+    tabla.className='table table-bordered table-striped table-hover';
     tabla.appendChild(crearCabecera(lista[0]));
     tabla.appendChild(crearCuerpo(lista));
     return tabla;
@@ -106,10 +182,44 @@ function agregarManejadorTr(tr){
     }
 }
 
+ function leerDatosDeLoco(list) {
+    console.log(localStorage.getItem(list));
+    return JSON.parse(localStorage.getItem(list));
+}
 
- 
 
-  localStorage.setItem("timer","tiempo");
-  localStorage.setItem("Anuncios",Anuncio);
-  
+function borrarDatosDelLoco(list) {
+
+    localStorage.removeItem(list);
+}
+function modificarAnuncio(anuncio) {
+
+    if (window.confirm("DESEA MODIFICAR ESTE ANUNCIO??")) {
+        let lista = leerDatosDeLoco("listarAutos");
+
+        anuncio = cargarDatos(frm, true);
+
+        lista.forEach(element => {
+            if (element.id == anuncio.id) {
+                element.titulo = anuncio.titulo;
+                element.transaccion = anuncio.transaccion;
+                element.descripcion = anuncio.descripcion;
+                element.precio = anuncio.precio;
+                element.num_puertas = anuncio.num_puertas;
+                element.num_KMs = anuncio.num_KMs;
+                element.potencia = anuncio.potencia;
+            }
+        });
+
+
+        borrarDatosDelLoco("listarAutos");
+        guardarDatosEnLoco("listarAutos", lista);
+        document.getElementById("btnBorrar").hidden = true;
+        document.getElementById("btnCancelar").hidden = true;
+
+        frm.removeEventListener('submit', manejadorModificar);
+
+    }
+
+}
   
